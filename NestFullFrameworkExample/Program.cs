acca02e26b899079d35c16e0b8585f31fcd17e32
Russ Cam <russ.cam@elastic.co>
2019-08-15 16:36:33 +1000

    Initial commit

---
+++ NestFullFrameworkExample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using Elastic.Managed.Ephemeral;
+using Nest;
+
+namespace NestFullFrameworkExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var version = "7.3.0";
+
+            var settings = new ConnectionSettings()
+                .DisableDirectStreaming()
+                .PrettyJson();
+            var client = new ElasticClient(settings);
+
+            using (var cluster = new EphemeralCluster(version, 1))
+            {
+                cluster.Start();
+                var rootNodeInfoResponse = client.RootNodeInfo();
+                Console.WriteLine(rootNodeInfoResponse.DebugInformation);
+            }
+
+            if (Debugger.IsAttached)
+                Console.ReadKey();
+        }
+    }
+}
